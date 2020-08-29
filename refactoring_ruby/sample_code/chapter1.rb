67b12d2940a37eb7b2418bc4fb20eca66169767f
uratomo <rop.ibuki@gmail.com>
2020-08-29 20:49:05 +0900

    moduleの抽出 && case文削除

--- refactoring_ruby/sample_code/chapter1.rb
+++ refactoring_ruby/sample_code/chapter1.rb
@@ -1,46 +1,53 @@
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
 
-# 商品情報
 class Movie
-  # どういうタイプの製品を持っているか?
   REGULAR = 0
   NEW_RELEASE = 1
   CHILDRENS = 2
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_writer :price
 
-  def price_code=(value)
-    @price_code = value
-    @price = case price_code
-      when REGULAR; RegularPrice.new
-      when NEW_RELEASE; NewReleasePrice.new
-      when CHILDRENS; ChildrensPrice.new
-    end
-  end
-
-  def initialize(title, the_price_code)
-    @title, self.price_code = title, the_price_code
+  def initialize(title, the_price)
+    @title, @price = title, the_price
   end
 
   def charge(days_rented)
     @price.charge(days_rented)
   end
+
+  def frequent_renter_points(days_rented)
+    @price.frequent_renter_points(days_rented)
+  end
 end
 
 class RegularPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result =2
     result += (days_rented - 2) * 1.5 if days_rented > 2
     result
   end
+end
 
 class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
 
 class ChildrensPrice
+  include DefaultPrice
+
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
@@ -55,8 +62,12 @@ class Rental
     @movie, @days_rented = movie, days_rented
   end
 
+  def charge
+    movie.charge(days_rented)
+  end
+
   def frequent_renter_points
-    movie.price_code == Movie.NEW_RELEASE && days_rented > 1 ? 2 : 1
+    movie.frequent_renter_points(days_rented)
   end
 end
 
@@ -74,18 +85,26 @@ class  Customer
     @rental << arg
   end
 
-  def html_statement
-    frequent_renter_points = 0
+  def statement
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
-    # 最終
     result += "Amount owed is #{total_charge}\n"
-    result += "you earned #{frequent_renter_points} frequent points"
+    result += "you earned #{total_frequent_renter_points} frequent points"
+    result
+  end
+
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      result += "\t" + each.movie.title + ": " + element.charge.to_s + "<br>\n"
+    end
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+      "frequent renter points<p>"
     result
   end
 
@@ -93,5 +112,9 @@ class  Customer
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) {|sum, rental| sum += rental.frequent_renter_points}
   end
 end
